1e67fb2f4dae3e03c0b019276f1c12ac8846054b
elston.lewis <elston.lewis@outscale.com>
2024-06-11 17:53:57 +0200

    Updated iso url for freebsd

--- scripts/base/freebsd.sh
+++ scripts/base/freebsd.sh
@@ -4,5 +4,5 @@ set -e
 #### BASIC IMAGE
 yum install -y wget qemu-img sg3_utils libgcrypt xz
 cd /tmp
-wget -O - https://download.freebsd.org/releases/VM-IMAGES/13.1-RELEASE/amd64/Latest/FreeBSD-13.1-RELEASE-amd64.qcow2.xz | unxz --decompress > FreeBSD-13.1-RELEASE-amd64.qcow2
+wget -O - https://download.freebsd.org/releases/VM-IMAGES/14.0-RELEASE/amd64/Latest/FreeBSD-14.0-RELEASE-amd64.qcow2.xz | unxz --decompress > FreeBSD-14.0-RELEASE-amd64.qcow2
 qemu-img convert ./*.qcow2 -O raw /dev/sda
